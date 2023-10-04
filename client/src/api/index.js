846b0b003826af4b5fc3efa53160d7c0749e5be9
AmezingHacker <125231810+SahilSarang@users.noreply.github.com>
2023-10-05 00:37:44 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stack-overflow-server-fb6q.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
